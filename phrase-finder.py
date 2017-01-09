facac947cc4f78b48ca5fc57b9bd77b4003b2623
Stephen Kelly <stevekm@users.noreply.github.com>
2017-01-08 23:59:04 -0500

    return top phrases but not the length I wanted

--- phrase-finder.py
+++ phrase-finder.py
@@ -26,7 +26,7 @@ def groom_line(line):
     line = re.sub('\s+', ' ', line).strip()
     return(line)
 
-def print_doc(text_file):
+def doc_dict(text_file, chunk_size):
     phrase_dict = defaultdict(int)
     phrase_list = []
     # read in the text
@@ -36,15 +36,16 @@ def print_doc(text_file):
     # split on sentences
     sentences_list = split_sentences(data)
 
-    # print the entire document
+    # process the entire document
     for sentence in sentences_list:
         sentence = groom_line(sentence)
-        sentence_phrases = make_chunk_list(sentence, 9, ' ')
+        sentence_phrases = chunk_comninator(sentence, chunk_size, ' ')
         if len(sentence_phrases) > 0:
             for phrase in sentence_phrases:
                 phrase_dict[phrase] += 1
-    for key, value in phrase_dict.items():
-        print value, '\t', key
+    return phrase_dict
+    # for key, value in phrase_dict.items():
+    #     print value, '\t', key
 
 def split_words(sentence):
     # split line into words
@@ -67,16 +68,51 @@ def make_chunk_list(line, chunk_size, split_char):
     chunk_list = []
     for m in re.finditer(split_char, line):
         if len(line[breakpoint:].split(split_char)) < chunk_size:
-            chunk_list.append(remove_caps(line[breakpoint:].strip()))
+            chunk = remove_caps(line[breakpoint:].strip())
+            chunk_list.append(chunk)
             break
         if index < chunk_size:
             index += 1
             continue
         else:
-            chunk_list.append(remove_caps(line[breakpoint:m.end()].strip()))
+            chunk = remove_caps(line[breakpoint:m.end()].strip())
+            chunk_list.append(chunk)
             index = 0
             breakpoint = m.end()
     return chunk_list
+
+def chunk_comninator(line, chunk_size, split_char):
+    remainder = len(line.split(split_char)) % chunk_size
+    remainder_count = 0
+    chunk_list = []
+    if int(remainder) != 0:
+        tmp_line = line
+        while remainder_count < remainder:
+            for chunk in make_chunk_list(tmp_line, chunk_size, split_char):
+                chunk_list.append(chunk)
+            tmp_line = tmp_line.split(split_char)
+            tmp_line.pop(0)
+            tmp_line = split_char.join(tmp_line)
+            remainder_count += 1
+    else:
+        for chunk in make_chunk_list(line, chunk_size, split_char):
+            chunk_list.append(chunk)
+    return chunk_list
+
+def sort_dict(doc_dict):
+    sorted(doc_dict.items(), key=lambda x: x[1])
+
+
+
+def my_debugger():
+    # DEBUGGING !!
+    # call with my_debugger() anywhere in your script
+    import readline # optional, will allow Up/Down/History in the console
+    import code
+    vars = globals().copy()
+    vars.update(locals())
+    shell = code.InteractiveConsole(vars)
+    shell.interact()
 
 # ~~~~ DO A THING ~~~~ #
 text_file="txt/Dracula-Bram_Stoker.txt"
@@ -86,6 +122,39 @@ line1 = "So much so that all the rest seemed to take courage as if infected some
 line2 = "All day long we seemed to dawdle through a country which was full of beauty of every kind. Sometimes we saw little towns or castles on the top of steep hills such as we see in old missals; sometimes we ran by rivers and streams which seemed from the wide stony margin on each side of them to be subject to great floods. It takes a lot of water, and running strong, to sweep the outside edge of a river clear. At every station there were groups of people, sometimes crowds, and in all sorts of attire."
 
 
-
+# my_debugger()
 # print split_sentences(line = line2)
-print_doc(text_file = text_file)
+# print_doc(text_file = text_file)
+
+# print chunk_comninator(line1, 4, ' ')
+text_dict = doc_dict(text_file, 5)
+bad_keys = []
+for key, value in text_dict.iteritems():
+    if int(value) < 2:
+        bad_keys.append(key)
+for key in bad_keys:
+    del text_dict[key]
+
+for key, value in sorted(text_dict.items(), key=lambda x: x[1], reverse=True):
+    print value, '\t', key
+# print sort_dict(text_dict)
+# phrase_dict = defaultdict(int)
+# phrase_list = []
+# for size in xrange(3,6):
+#     phrases = make_chunk_list(line1, size, ' ')
+#     print "PHRASES ARE:", phrases
+#     for phrase in phrases:
+#         print "PHRASE IS:", phrase
+#         # phrase_list.append(phrase)
+#         # print "\n".join(s for s in mylist if sub.lower() in s.lower())
+#         if len(phrase_list) < 1 :
+#             phrase_list.append(phrase)
+#         for item in phrase_list:
+#             print "ITEM IS:", item
+#             if phrase not in item:
+#                 print phrase
+#                 phrase_list.append(phrase)
+#         # phrase_dict[phrase] += 1
+#
+# for key, value in phrase_dict.items():
+#     print value, '\t', key
